cfa5dbbd8fad70ca1b59caab3e39d2452bea7ed9
Weijian Huang <31432912+SZUHvern@users.noreply.github.com>
2021-02-17 08:59:49 +0800

    Update registration.py

--- registration.py
+++ registration.py
@@ -189,7 +189,7 @@ def field_visualization(field, path=None):
 if __name__ == "__main__":
 
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-    path_h5_save = '/media/siat/770f90d2-a17b-4301-aa9f-ca2616ede7ec/TMI_reestablish/ori_data/deal/'
+    path_h5_save = './ori_data/deal/'
     load_weight = './model/affine/epoch003-0.76.h5' #should load the affine weight
     mode = 'train'
 
